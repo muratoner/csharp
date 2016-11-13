729cd7fb610fbffb07a2c84099e8d95b41857f19
Murat ÖNER <muhackgames@gmail.com>
2016-11-13 18:55:10 +0200

    Added facebook project.

---
+++ MHG.Facebook/Program.cs
@@ -0,0 +1,14 @@
+﻿using Facebook;
+
+namespace MHG.Facebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://developers.facebook.com/docs/facebook-login/access-tokens#extending
+            var fb = new FacebookClient("[accessToken]");
+            var res = fb.Get("/me");
+        }
+    }
+}
