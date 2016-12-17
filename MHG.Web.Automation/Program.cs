564afc5c45fd114f8ede8b57535a5f55bd5d5632
Murat <muhackgames@gmail.com>
2016-12-17 21:44:58 +0300

    Selenium Web Automation'unun kullanıldığı MHG.Web.Automation adlı projeyi ekledik.

---
+++ MHG.Web.Automation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MHG.Web.Automation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chrome = new OpenQA.Selenium.Chrome.ChromeDriver { Url = "http://www.google.com" };
+            chrome.FindElementById("lst-ib").SendKeys("muratoner.net");
+            chrome.FindElementByClassName("lsb").Click();
+        }
+    }
+}
