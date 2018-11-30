4b540d8680bdecec0a7703bd4d01afd7e3d6c10d
Murat ÖNER <muhackgames@gmail.com>
2018-11-30 09:03:15 +0300

    Merged with csharp6 repository.

---
+++ Csharp6/MHG.AutoPropertyInitializer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MHG.AutoPropertyInitializer
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            Console.WriteLine(person.Age);
+            Console.WriteLine(person.City);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Auto Property Initilaizer tanımı ister get isterse get ve set tanımlı özelliklerde kullanılabiliyor.
+        /// </summary>
+        #region Auto Property Initializer
+        class Person
+        {
+            public int Age { get; set; } = 99;
+            public string City { get; } = "Kocaeli";
+        }
+        #endregion
+    }
+}
---
+++ Csharp6/MHG.PrimaryConstructors/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MHG.PrimaryConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+
+    #region Primary Constructor
+    class Person(int age, string city)
+    {
+
+    }
+    #endregion
+}
