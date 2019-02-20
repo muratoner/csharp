105de6efd08bf2aca7d568e304bfa2bb5e4e8b4b
Murat ÖNER <info@muratoner.net>
2019-02-20 12:28:54 +0300

    New MHG.Selenium.Mobile project added.

---
+++ MHG.Selenium.Mobile/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace MHG.Selenium.Mobile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.EnableMobileEmulation("Nexus 7");
+            using (var chromeMobile = new ChromeDriver(chromeOptions))
+            {
+                chromeMobile.Navigate().GoToUrl("https://www.instagram.com");
+            }
+        }
+    }
+}
